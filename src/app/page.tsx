dbc5467cb73d48a997d0576abd4a298585eb3e91
kennyAnyi9 <kennyanyi9@gmail.com>
2023-07-29 06:55:43 +0000

    fix: reposition button and add more blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
             alt="Login"
           />{" "}
         </div>
-        <div className="text-3xl w-10 h-10 flex items-center mr-5 justify-center rounded-full ring-1 ring-gray-500">
+        <div className="text-3xl w-10 h-10 text-white z-10 flex items-center mr-5 justify-center rounded-full ring-1 ring-gray-500">
           <HiOutlineMenuAlt4 />
         </div>
       </nav>
@@ -48,8 +48,8 @@ export default function Home() {
           <span>#Discover</span>
           <span>#Grow</span>
         </div>
-        <div className="relative flex  w-4/5">
-          <div className="absolute inset-0 top-5 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur-lg opacity-50"></div>
+        <div className="relative flex items-center justify-center w-4/5">
+          <div className="absolute inset-0 top-5 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur-xl opacity-50"></div>
           <button
             className=" relative px-3 py-4 bg-black rounded-lg ring-1 ring-pink-600 mt-5 
                         leading-none flex items-center divide-x "

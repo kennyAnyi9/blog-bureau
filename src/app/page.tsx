420101cafb40b9b051bb37d66984d063bcbc92d1
kennyAnyi9 <kennyanyi9@gmail.com>
2023-08-01 17:21:12 +0000

    style: modify page to support dark and light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,55 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { HiOutlineMenuAlt4 } from "react-icons/hi";
 import { FcAlarmClock, FcBullish, FcMindMap, FcOrgUnit } from "react-icons/fc";
 import { DiGithubBadge } from "react-icons/di";
 import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
-import { BsFillRocketTakeoffFill, BsStars } from "react-icons/bs";
+import { BsStars } from "react-icons/bs";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
   return (
-    <div className="relative   bg-black flex flex-col items-center  min-h-screen ">
-      <nav className="w-full z-10 backdrop-filter backdrop-blur-md bg-opacity-50 fixed md:w-3/5 h-20 bg-black   flex flex-row items-center  justify-between ">
-        <div className="flex flex-row w-1/3 mx-5 lg:mx-0">
+    <div className="relative    flex flex-col items-center  min-h-screen ">
+      <nav className="w-full z-10 backdrop-filter backdrop-blur-md bg-opacity-50 fixed lg:w-3/5 h-20 dark:bg-black   flex flex-row items-center  justify-between ">
+        <div className="flex flex-row w-1/3 mx-5  lg:mx-0">
           {" "}
-          <Image
-            width={50}
-            height={50}
-            src="/images/Bd-1-white.png"
-            alt="Login"
-          />{" "}
+          {theme === "dark" ? (
+            <Image
+              width={50}
+              height={50}
+              src="/images/Bd-1-white.png"
+              alt="Login"
+              className="cursor-pointer"
+            />
+          ) : (
+            <Image
+              width={50}
+              height={50}
+              src="/images/Bd-1-black.png"
+              alt="Login"
+              className="cursor-pointer"
+            />
+          )}
         </div>
-        <div className="hidden md:flex flex-row justify-around w-1/3">
-          <Link href="#" className="pr-5">
-            /blog
+        <div className="hidden md:flex text-black dark:text-white flex-row justify-around w-1/3">
+          <Link href="#" className=" pr-5 ">
+            / blog
           </Link>
           <Link href="#" className="pr-5">
-            /download
+            / download
           </Link>
           <Link href="#" className="pr-5">
-            /portfolio
+            / portfolio
           </Link>
         </div>
         <div className="hidden md:flex flex-row justify-around w-1/3  ">
           <div className=" flex p-1 divide-x divide-white space-x-2 flex-row text-2xl w-fit h-fit rounded-2xl ring-1 ring-gray-500">
-            <div className="flex space-x-2 text-white flex-row">
-              <MdOutlineDarkMode />
-              <MdOutlineLightMode />
+            <div className="flex space-x-2 text-black cursor-pointer  dark:text-white flex-row">
+              <MdOutlineDarkMode onClick={() => setTheme("dark")} />
+
+              <MdOutlineLightMode onClick={() => setTheme("white")} />
             </div>
           </div>
         </div>
@@ -47,16 +62,17 @@ export default function Home() {
         {" "}
         <div className="flex flex-col lg:flex-row">
           <h1
-            className="text-7xl  font-bold shadow-xl
-                    text-transparent bg-clip-text
-                   text-white
+            className="text-7xl  font-extrabold dark:shadow-xl
+                     bg-clip-text dark:text-white
+                    text-black
+                    
                     text-center
                      "
           >
             Blog.
           </h1>
           <h1
-            className="text-7xl md:text-9xl font-bold shadow-xl
+            className="text-7xl md:text-9xl font-bold dark:shadow-xl
                     text-transparent bg-clip-text
                     bg-gradient-to-r from-purple-600 to-pink-600
                     text-center
@@ -66,30 +82,36 @@ export default function Home() {
           </h1>
         </div>
         <div className="w-full">
-          <p className="flex text-xl flex-col text-center text-gray-400 mt-10">
+          <p className="flex text-xl flex-col text-center text-gray-700 dark:text-gray-400 mt-10">
             Unleash the power of books through powerful summaries, life changing
             insights & personal reflections
           </p>
         </div>
         <div className="flex flex-col mt-10 md:flex-row justify-center items-center  md:space-x-5 ">
           <button
-            className=" relative px-4 py-3  rounded-lg bg-white shadow-md drop-shadow-sm mt-5 
-                        leading-none flex items-center divide-x ring-1 ring-white "
+            className=" relative px-4 py-3  rounded-lg bg-black dark:bg-white shadow-md drop-shadow-sm mt-5 
+                        leading-none flex items-center divide-x ring-1 ring-black dark:ring-white "
           >
             <span className="px-20 md:px-8 flex items-center space-x-2">
-              <span className=" text-black font-bold">Discover </span>
-              <span className=" text-black font-bold">More </span>
+              <span className=" text-white dark:text-black font-bold">
+                Discover{" "}
+              </span>
+              <span className=" text-white dark:text-black font-bold">
+                More{" "}
+              </span>
             </span>
           </button>
           <div className="relative flex items-center justify-center w-64">
             <div className="absolute -inset-2 top-5 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur-xl opacity-50"></div>
             <button
-              className=" relative px-3 py-3 bg-black rounded-lg ring-1 ring-pink-600 mt-5 
+              className=" relative px-3 py-3 bg-balck bg-white dark:bg-black rounded-lg ring-1 ring-pink-600 mt-5 
                         leading-none flex items-center divide-x "
             >
               <span className="px-20 md:px-8  flex items-center space-x-2">
-                <span className=" text-gray-100">Uncover </span>
-                <span className=" text-gray-100">Insights </span>
+                <span className=" text-black dark:text-gray-100">Uncover </span>
+                <span className="  text-black dark:text-gray-100">
+                  Insights{" "}
+                </span>
               </span>
             </button>
           </div>
@@ -101,14 +123,14 @@ export default function Home() {
               <h1 className="text-black font-extrabold">1</h1>
             </div>
             <h1
-              className="font-bold shadow-xl mt-5 text-4xl
+              className="font-bold dark:shadow-xl mt-5 text-4xl
                     text-transparent bg-clip-text
                     bg-gradient-to-r from-blue-600 to-blue-400
                     text-center"
             >
               Discover
             </h1>
-            <p className="text-center mt-5 text-gray-200 text-xl">
+            <p className="text-center mt-5 text-black dark:text-gray-200 text-xl">
               Explore the boundless potential of understanding in Neurology and
               Psychology, encompassing the influence of the subconscious mind,
               neurotransmitters, and brain functionality.{" "}
@@ -121,14 +143,14 @@ export default function Home() {
               <h1 className="text-black font-extrabold">2</h1>
             </div>
             <h1
-              className="font-bold shadow-xl mt-5 text-4xl
+              className="font-bold dark:shadow-xl mt-5 text-4xl
                     text-transparent bg-clip-text
                     bg-gradient-to-r from-purple-600 to-pink-800
                     text-center"
             >
               Learn
             </h1>
-            <p className="text-center mt-5 text-gray-200 text-xl">
+            <p className="text-center mt-5 text-black dark:text-gray-200 text-xl">
               Embark on a journey of continuous learning and personal
               development. Uncover the fascinating realms of Neurology and
               Psychology, delving into the intricate workings of the
@@ -143,14 +165,14 @@ export default function Home() {
               <h1 className="text-black font-extrabold">3</h1>
             </div>
             <h1
-              className="font-bold shadow-xl mt-5 text-4xl
+              className="font-bold dark:shadow-xl mt-5 text-4xl
                     text-transparent bg-clip-text
                     bg-gradient-to-r from-orange-700 to-orange-400
                     text-center"
             >
               Grow
             </h1>
-            <p className="text-center  mt-5 text-gray-200 text-xl">
+            <p className="text-center  mt-5 text-black dark:text-gray-200 text-xl">
               Nurture your personal growth and evolution. Unlock the potential
               within yourself as you explore the realms of Neurology and
               Psychology. Discover the profound impact of the subconscious mind,
@@ -163,7 +185,7 @@ export default function Home() {
         </div>
         <div className="mt-20 ">
           <h1
-            className="text-5xl text-center lg:text-center text-white text-transparent font-bold bg-clip-text
+            className="text-5xl text-center lg:text-center text-black dark:text-white  font-bold bg-clip-text
                     "
           >
             Powerful Book{" "}
@@ -174,7 +196,7 @@ export default function Home() {
             >
               Summaries{" "}
             </span>
-            <span className="text-white">and </span>
+            <span className="text-black dark:text-white">and </span>
             <span
               className="text-transparent bg-clip-text 
                     bg-gradient-to-t from-blue-700 to-pink-600"
@@ -182,7 +204,7 @@ export default function Home() {
               Personal Reflections
             </span>
           </h1>
-          <p className="mt-10 text-xl text-center text-gray-300">
+          <p className="mt-10 text-xl text-center text-black dark:text-gray-300">
             Empowering insights in concise form. Unleash the potential of
             powerful book summaries and personal reflections. Discover a wealth
             of knowledge distilled into bite-sized wisdom, ready to inspire and
@@ -197,11 +219,11 @@ export default function Home() {
             <div className="m-7">
               <h1
                 className="text-2xl mb-5 font-bold text-transparent bg-clip-text 
-                    bg-gradient-to-r from-blue-400 to-emerald-800 "
+                    bg-gradient-to-r from-blue-700 to-emerald-900 "
               >
                 Self Discipline
               </h1>
-              <p className="text-gray-400">
+              <p className="text-black dark:text-gray-400">
                 {" "}
                 Embark on a transformative journey to master self-discipline.
                 Gain valuable insights, practical tips, and introspective
@@ -219,11 +241,11 @@ export default function Home() {
             <div className="m-7">
               <h1
                 className="text-2xl mb-5 font-bold text-transparent bg-clip-text 
-                    bg-gradient-to-r from-emerald-400 to-emerald-900 "
+                    bg-gradient-to-r from-emerald-700 to-emerald-900 "
               >
                 Daily Habits
               </h1>
-              <p className="text-gray-400">
+              <p className="text-black dark:text-gray-400">
                 {" "}
                 Embark on a life-changing journey to transform your habits.
                 Discover powerful insights, actionable tips, and introspective
@@ -241,11 +263,11 @@ export default function Home() {
             <div className="m-7">
               <h1
                 className="text-2xl mb-5 font-bold text-transparent bg-clip-text 
-                    bg-gradient-to-r from-blue-400 to-pink-900 "
+                    bg-gradient-to-r from-blue-800 to-pink-900 "
               >
                 The Compound Effect
               </h1>
-              <p className="text-gray-400">
+              <p className="text-black dark:text-gray-400">
                 {" "}
                 Experience the profound impact of the compound effect through
                 our enlightening content. Uncover how small, consistent actions
@@ -264,11 +286,11 @@ export default function Home() {
             <div className="m-7">
               <h1
                 className="text-2xl mb-5 font-bold text-transparent bg-clip-text 
-                    bg-gradient-to-r from-orange-600 to-orange-900 "
+                    bg-gradient-to-r from-orange-500 to-orange-900 "
               >
                 Time Management
               </h1>
-              <p className="text-gray-400">
+              <p className="text-black dark:text-gray-400">
                 {" "}
                 Optimize your life with effective time management techniques.
                 Delve into our insightful content and discover valuable
@@ -288,10 +310,10 @@ export default function Home() {
             {" "}
             <h1
               className="text-7xl text-center  font-bold text-transparent bg-clip-text
-                    bg-gradient-to-r from-blue-600 to-blue-400 mt-20 mb-28"
+                    bg-gradient-to-r from-blue-800 to-blue-500 mt-20 mb-28"
             >
               Featured Books
-              <span className="block text-xl  text-gray-300 mt-2">
+              <span className="block text-xl  text-black dark:text-gray-300 mt-10">
                 Discover our collection of powerful book summaries and personal
                 reflections on a wide range of topics. From self-development to
                 neuroscience, we have curated a selection of thought-provoking
@@ -300,7 +322,7 @@ export default function Home() {
               </span>
             </h1>
           </div>
-          <div className=" w-full lg:w-1/2 rounded-xl h-fit bg-zinc-950 ring-2 ring-zinc-950 flex flex-col">
+          <div className=" w-full lg:w-1/2 rounded-xl h-fit bg-gray-200  dark:bg-zinc-950 dark:ring-2 ring-zinc-950 flex flex-col">
             <div className="flex justify-center items-center">
               <Image
                 layout="responsive"
@@ -315,26 +337,26 @@ export default function Home() {
             <div className="p-10 space-y-10">
               <h1
                 className="font-bold text-2xl text-transparent bg-clip-text
-                    bg-gradient-to-r from-blue-600 to-blue-400"
+                    bg-gradient-to-r from-blue-800 to-blue-500"
               >
                 Atomic Habits - James Clear
               </h1>
               <h1 className="flex flex-row justify-between text-lg font-bold">
                 Powerful Insights
-                <span className="flex flex-row text-2xl text-yellow-500">
+                {/* <span className="flex flex-row text-2xl text-yellow-500">
                   <BsStars />
                   <BsStars />
                   <BsStars />
                   <BsStars />
-                </span>
+                </span> */}
               </h1>
-              <div className="text-gray-300">
+              <div className=" text-black dark:text-gray-300 text-justify">
                 James Clear presents practical strategies for creating positive
                 habits and breaking free from negative ones. By understanding
                 the science behind habit formation, readers can make incremental
                 changes that lead to significant personal growth and success.
               </div>
-              <button className="rounded-3xl bg-blue-700 px-5 py-2 drop-shadow-md shadow-xl">
+              <button className="rounded-3xl ring-1  dark:ring-blue-700 transition duration-150 ease-in-out hover:bg-blue-600 px-5 py-2 drop-shadow-md shadow-xl">
                 Start Reading
               </button>
             </div>
@@ -358,8 +380,9 @@ export default function Home() {
         <div className="flex mb-10 flex-col mt-3 items-center">
           <div className=" flex p-1 divide-x divide-gray-600 space-x-2 flex-row text-2xl w-fit h-fit rounded-2xl ring-1 ring-gray-500">
             <div className="flex space-x-2 text-gray-500 flex-row">
-              <MdOutlineDarkMode />
-              <MdOutlineLightMode />
+              <MdOutlineDarkMode onClick={() => setTheme("dark")} />
+
+              <MdOutlineLightMode onClick={() => setTheme("white")} />
             </div>
             <div className="pl-2 text-gray-500">
               <DiGithubBadge />
